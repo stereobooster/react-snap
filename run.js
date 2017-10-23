c619f6cebdc06803e7a6ff7ea3d844932bdad8f3
stereobooster <stereobooster@gmail.com>
2017-10-23 13:57:20 +0200

    Fix stupid error

--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 const { run } = require("./index.js");
-const { userOptions } = require(`${process.cwd()}/package.json`);
+const { reactSnap } = require(`${process.cwd()}/package.json`);
 
-run(userOptions);
+run(reactSnap);
