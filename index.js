fbf4a7f7cbdc5ff4dc5ef487bd3b425f929cbc1d
stereobooster <stereobooster@gmail.com>
2017-11-16 04:12:41 +0100

    [#36] stop processing and exit in case of error

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ const inlineCss = async opt => {
     userAgent: options.userAgent
   });
   const cssText = minimalcssResult.finalCss;
-  console.log("inline css", cssText.length);
+  // console.log("inline css", cssText.length);
   return page.evaluate(
     (cssText, preloadPolyfill) => {
       var head = document.head || document.getElementsByTagName("head")[0],
@@ -336,7 +336,7 @@ const run = async userOptions => {
           options,
           basePath,
           browser
-        });
+        }).catch(e => console.log(1));
       if (options.fixWebpackChunksIssue) {
         await fixWebpackChunksIssue({
           page,
