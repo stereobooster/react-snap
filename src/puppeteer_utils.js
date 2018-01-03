e64428943554d91f9424249dd4b845e1066873a9
lindongwu <lindongwu@memect.co>
2018-01-03 16:39:06 +0800

    Add executablePath to options

--- src/puppeteer_utils.js
+++ src/puppeteer_utils.js
@@ -135,6 +135,7 @@ const crawl = async opt => {
   const browser = await puppeteer.launch({
     headless: options.headless,
     args: options.puppeteerArgs,
+    executablePath: options.executablePath,
     handleSIGINT: false
   });
 
