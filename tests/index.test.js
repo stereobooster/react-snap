b06de435bc687273f0df4268edce8f9cb4ca76f0
stereobooster <stereobooster@gmail.com>
2018-05-05 18:00:39 +0200

    improve tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,11 +9,20 @@ const DevNullStream = require("dev-null-stream");
 const devNullStream = new DevNullStream();
 
 const cwd = process.cwd();
+const createReadStreamMock = jest.fn();
+const createWriteStreamMock = jest.fn();
 const writeFileSyncMock = jest.fn();
+
 const fs = {
   existsSync: nativeFs.existsSync,
-  createReadStream: nativeFs.createReadStream,
-  createWriteStream: (...args) => devNullStream,
+  createReadStream: path => {
+    createReadStreamMock(path.replace(cwd, ""));
+    return nativeFs.createReadStream(path);
+  },
+  createWriteStream: path => {
+    createWriteStreamMock(path.replace(cwd, ""));
+    return devNullStream;
+  },
   writeFileSync: (path, content) => {
     writeFileSyncMock(path.replace(cwd, ""), content);
   }
@@ -23,16 +32,28 @@ const { run } = require("./../index.js");
 
 rewiremock.enable();
 
-test("one page test", async () => {
-  await run(
-    {
-      source: "tests/examples/one-page",
-      puppeteerArgs: ['--no-sandbox', '--disable-setuid-sandbox']
-    },
-    {
-      fs
-    }
-  );
-  expect(writeFileSyncMock.mock.calls.length).toEqual(1);
-  expect(writeFileSyncMock.mock.calls).toMatchSnapshot();
+describe("one page", async () => {
+  const source = "tests/examples/one-page";
+  beforeAll(async () => {
+    await run(
+      {
+        source,
+        puppeteerArgs: ["--no-sandbox", "--disable-setuid-sandbox"]
+      },
+      {
+        fs
+      }
+    );
+  });
+  test("proceeds index.html", () => {
+    expect(writeFileSyncMock.mock.calls.length).toEqual(1);
+    expect(writeFileSyncMock.mock.calls[0][0]).toEqual(`/${source}/index.html`);
+    expect(writeFileSyncMock.mock.calls[0][1]).toMatchSnapshot();
+  });
+  test("copies (original) index.html to 200.html", () => {
+    expect(createReadStreamMock.mock.calls).toEqual([
+      [`/${source}/index.html`]
+    ]);
+    expect(createWriteStreamMock.mock.calls).toEqual([[`/${source}/200.html`]]);
+  });
 });
