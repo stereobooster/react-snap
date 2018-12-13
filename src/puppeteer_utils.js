41920adc152eb58d4e417bccdf2145ccde1b2e4c
stereobooster <stereobooster@gmail.com>
2018-12-13 16:33:47 +0100

    [#314] disable ServiceWorker

--- src/puppeteer_utils.js
+++ src/puppeteer_utils.js
@@ -211,6 +211,7 @@ const crawl = async opt => {
     if (!shuttingDown && !skipExistingFile) {
       try {
         const page = await browser.newPage();
+        await page._client.send("ServiceWorker.disable");
         await page.setCacheEnabled(options.puppeteer.cache);
         if (options.viewport) await page.setViewport(options.viewport);
         if (options.skipThirdPartyRequests)
